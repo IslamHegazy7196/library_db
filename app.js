80d6b689c522c214e09f109952bb2dec8e2a6900
islam hegazy <islamfathihegazy04@gmail.com>
2023-12-11 17:04:44 +0200

    feat: add sequelize

--- app.js
+++ app.js
@@ -1,29 +1,22 @@
 const express=require('express')
 const app =express()
 const bodyParser = require('body-parser');
-const { Pool } = require('pg');
+const Book = require('./models/book');
+const Borrower = require('./models/borrower');
+const BorrowingProcess = require('./models/borrowingProcess');
 
 
 app.get('/',(req,res)=>{res.send('bosta library management system')})
 
 app.use(bodyParser.json());
 
-const pool = new Pool({
-    user: 'postgres',
-    host: '127.0.0.1',
-    database: ' bostalib_db',
-    password: '696969',
-    port: 5432,
-});
-
-
 
 const port= 5000
-
-
-
 
 const start=()=>{    
         app.listen(port,()=>{console.log(`server is listening on port ${port}`)})
 }
 start()
+
+
+
